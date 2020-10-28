f1443bf66a4dd22d7b545d1eadfeb288db6101c2
bwilder95 <brentonwilder1995@gmail.com>
2020-10-28 15:22:09 -0700

    bring in data to python

--- python/Data_Explore.py
+++ python/Data_Explore.py
@@ -9,4 +9,9 @@ print(fdt.head())
 # Load the sentiment library
 url2 = 'https://github.com/wpmcgrath95/TrumpTweetsSentimentAnalysis/blob/data_and_feat_engineering/data/sentLib.csv?raw=true'
 sentlib = pd.read_csv(url2,sep=',')  
+
+# Split on # symbol to isolate sentiment words
+sentlib[['Lemma','PoS']] = sentlib["# lemma#PoS"].str.split("#", 1, expand=True)
 print(sentlib.head())
+
+
