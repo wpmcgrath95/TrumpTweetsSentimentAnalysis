d19ab806d1d9507f57069762e1e01cf7b64aaca7
bwilder95 <brentonwilder1995@gmail.com>
2020-10-28 19:34:11 -0700

    stuck how to test polarity for each row

--- python/Data_Explore.py
+++ python/Data_Explore.py
@@ -1,23 +1,29 @@
 # Import Libraries
+import sys
 import pandas as pd
 
-# Load the trump tweets directly from repository
-url = 'https://github.com/wpmcgrath95/TrumpTweetsSentimentAnalysis/blob/data_and_feat_engineering/data/realdonaldtrump.csv?raw=true'
-fdt = pd.read_csv(url,sep=',')
-print(fdt.head())
+def main():
+    # Load the trump tweets directly from repository
+    url = 'https://github.com/wpmcgrath95/TrumpTweetsSentimentAnalysis/blob/data_and_feat_engineering/data/realdonaldtrump.csv?raw=true'
+    fdt = pd.read_csv(url,sep=',')
+    print(fdt.head())
 
-# Load the sentiment library
-url2 = 'https://github.com/wpmcgrath95/TrumpTweetsSentimentAnalysis/blob/data_and_feat_engineering/data/sentLib.csv?raw=true'
-sentlib = pd.read_csv(url2,sep=',')  
+    # Load the sentiment library
+    url2 = 'https://github.com/wpmcgrath95/TrumpTweetsSentimentAnalysis/blob/data_and_feat_engineering/data/sentLib.csv?raw=true'
+    sentlib = pd.read_csv(url2,sep=',')  
 
-# Split on '#' symbol to isolate sentiment words
-sentlib[['Lemma','PoS']] = sentlib["# lemma#PoS"].str.split("#", 1, expand=True)
+    # Split on '#' symbol to isolate sentiment words
+    sentlib[['Lemma','PoS']] = sentlib["# lemma#PoS"].str.split("#", 1, expand=True)
   
-# Convert sentiment library to dictionary 
-sent = dict(zip(sentlib.Lemma, sentlib.prior_polarity_score))
+    # Convert sentiment library to dictionary 
+    sent = dict(zip(sentlib.Lemma, sentlib.prior_polarity_score))
 
-# Split the tweets by each word
+    # Calculate polarity for each tweet (THIS IS WRONG)
+    for row in fdt['content']:
+        if any((word in row) for word in sent):
+            fdt['polarity'] = sum(sent)
 
 
+if __name__ == "__main__":
+    sys.exit(main())
 
-# Write a function that assess the average sentiment for each tweet
