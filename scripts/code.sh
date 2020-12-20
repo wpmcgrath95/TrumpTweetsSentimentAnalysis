ce7f4eb01861509206c00f8ef6c69437cdde8404
wpmcgrath95 <wpmcgrath95@gmail.com>
2020-12-20 11:13:33 -0800

    updated Dockerfile

--- scripts/code.sh
+++ scripts/code.sh
@@ -8,4 +8,4 @@ mkdir -p ./data/
 python3 ./python/LDA_Grouping.py
 python3 ./python/Text_Blob.py
 python3 ./python/Labeling_Model.py
-#python3 ./python/Cluster_Model.py
+python3 ./python/Cluster_Model.py
